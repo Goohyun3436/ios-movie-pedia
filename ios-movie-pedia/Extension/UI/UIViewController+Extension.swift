63656768d420bb31188148dc7dc9afda015d182a
Goohyun3436 <3436hyun@gmail.com>
2025-01-25 03:47:40 +0900

    feat: 온보딩 화면 - 최초로 앱을 실행한 경우, 프로필 설정하지 않은 경우 진입

--- ios-movie-pedia/Extension/UI/UIViewController+Extension.swift
+++ ios-movie-pedia/Extension/UI/UIViewController+Extension.swift
@@ -16,18 +16,4 @@ extension UIViewController {
             UserDefaults.standard.setValue(encoded, forKey: forKey)
         }
     }
-    
-    func loadJsonData<T: Codable>(type: T.Type, forKey: String) -> T? {
-        guard let savedData = UserDefaults.standard.object(forKey: forKey) as? Data else {
-            return nil
-        }
-        
-        let decoder = JSONDecoder()
-        
-        guard let savedObject = try? decoder.decode(T.self, from: savedData) else {
-            return nil
-        }
-        
-        return savedObject
-    }
 }
--- ios-movie-pedia/SceneDelegate.swift
+++ ios-movie-pedia/SceneDelegate.swift
@@ -18,9 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-//        window?.rootViewController = TabBarController()
-        window?.rootViewController = UINavigationController(rootViewController: OnboardingViewController())
-//        window?.rootViewController = SettingProfileViewController()
+        
+        let profile = loadJsonData(type: Profile.self, forKey: "profile")
+        
+        window?.rootViewController = profile == nil
+            ? UINavigationController(rootViewController: OnboardingViewController())
+            : TabBarController()
+        
         window?.makeKeyAndVisible()
     }
 
