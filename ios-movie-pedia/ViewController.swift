b25db02a6daa7855b47f23e6c3ba6f7e47a3f788
Goohyun3436 <3436hyun@gmail.com>
2025-01-24 14:12:05 +0900

    Initial Commit

---
+++ ios-movie-pedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-movie-pedia
+//
+//  Created by Goo on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
