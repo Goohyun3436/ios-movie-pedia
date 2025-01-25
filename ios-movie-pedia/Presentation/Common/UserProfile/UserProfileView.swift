af25841832077bdde7966ad2e952de66eb66d358
Goohyun3436 <3436hyun@gmail.com>
2025-01-25 21:03:21 +0900

    design: 메인 화면 - 유저 프로필 영역 UI

--- ios-movie-pedia/Presentation/Common/UserProfile/UserProfileView.swift
+++ ios-movie-pedia/Presentation/Common/UserProfile/UserProfileView.swift
@@ -21,7 +21,6 @@ final class UserProfileView: BaseView {
     func configureData(_ profile: Profile) {
         profileImage.configureData(profile.image)
         nicknameLabel.text = profile.nickname
-        nicknameLabel.text = "세상에서가장긴닉네임이존재한다면아마두줄세줄네줄다섯줄여섯줄이되겠죠"
         createdAtLabel.text = profile.createdAtFormat
     }
     
@@ -36,41 +35,50 @@ final class UserProfileView: BaseView {
     
     override func configureLayout() {
         profileImage.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(16)
-            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(12)
+            make.top.equalToSuperview().offset(12)
         }
         
         nicknameLabel.snp.makeConstraints { make in
             make.leading.equalTo(profileImage.snp.trailing).offset(12)
             make.trailing.lessThanOrEqualTo(rightButton.snp.leading).offset(-12)
-            make.top.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(18)
         }
         
         createdAtLabel.snp.makeConstraints { make in
             make.leading.equalTo(profileImage.snp.trailing).offset(12)
-            make.top.equalTo(nicknameLabel.snp.bottom).offset(8)
+            make.top.equalTo(nicknameLabel.snp.bottom).offset(4)
         }
         
         rightButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().inset(16)
+            make.trailing.equalToSuperview().inset(12)
+            make.top.equalToSuperview().offset(12)
             make.centerY.equalTo(profileImage.snp.centerY)
-            make.size.equalTo(30)
+            make.width.equalTo(30)
+            make.bottom.equalTo(bottomButton.snp.top).offset(-16)
         }
         
         bottomButton.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(16)
+            make.horizontalEdges.equalToSuperview().inset(12)
             make.top.greaterThanOrEqualTo(profileImage.snp.bottom).offset(16)
-            make.top.equalTo(createdAtLabel.snp.bottom).offset(16)
+            make.top.equalTo(createdAtLabel.snp.bottom).offset(22)
             make.height.equalTo(40)
-            make.bottom.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(12)
         }
     }
     
     override func configureView() {
+        layer.cornerRadius = 16
         backgroundColor = AppColor.tertiaryBackgroundColor
+        
         nicknameLabel.numberOfLines = 0
-        nicknameLabel.backgroundColor = .orange
+        nicknameLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        
+        createdAtLabel.font = UIFont.systemFont(ofSize: 12)
+        createdAtLabel.textColor = AppColor.darkgray
+        
         rightButton.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        rightButton.contentHorizontalAlignment = .trailing
         rightButton.tintColor = AppColor.darkgray
     }
     
